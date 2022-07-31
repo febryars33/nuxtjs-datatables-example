6611a4e64dc8e31c71a38b0c479a2b551834bd3a
febryars33 <febriansyahrikisetiadi@gmail.com>
2022-07-31 19:43:49 +0700

    First commit

---
+++ pages/datatables.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container mt-5">
+    <h2>NuxtJS With jQuery DataTables</h2>
+    <div class="table-responsive">
+      <table id="datatables" class="table table-hover table-striped">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Office</th>
+            <th>Age</th>
+            <th>Start Date</th>
+            <th>Salary</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'DataTablesPage',
+  mounted () {
+    $('#datatables').DataTable()
+  }
+})
+</script>
